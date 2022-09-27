fdc5e58a7d1870deeaae77beb9b69b79caf17b48
olihock <oligithub01>
2022-09-27 23:16:10 +0200

    test csv loader

--- src/test/java/de/ithoc/datafetching/sensorcommunity/FileLoaderTest.java
+++ src/test/java/de/ithoc/datafetching/sensorcommunity/FileLoaderTest.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,11 +40,38 @@ class FileLoaderTest {
     }
 
     @Test
+    void read_csv_from_subfolder() throws IOException, URISyntaxException {
+        String read = read("2015-10-01/2015-10-01_ppd42ns_sensor_27.csv");
+        Assertions.assertNotNull(read);
+    }
+
+    @Test
     void download_csv_files() throws IOException, URISyntaxException {
         String read = read("date-list-shortened.html");
         List<String> strings = FileLoader.filterDates(read);
+        Assertions.assertEquals(3, strings.size());
+
         strings.forEach(date -> {
-            System.out.println(date);
+            try {
+                String fileName = date + ".html";
+                String content = read(fileName);
+                List<String> csvFiles = FileLoader.filterCsvFiles(content);
+                Assertions.assertTrue(csvFiles.size() > 0);
+
+                csvFiles.forEach(csv -> {
+                    String file = date + "/" + csv;
+                    try {
+                        String csvContent = read(file);
+                        Assertions.assertNotNull(csvContent);
+
+                    } catch (IOException | URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
         });
     }
 
