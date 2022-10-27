6ac346b692ddb620f074aab818b60561e7b28407
Oliver Hock <oliver.hock@mecom.de>
2022-10-27 14:08:30 +0200

    change sensor hypothesis

--- matlab/sensor-hypothesis.m
+++ matlab/sensor-hypothesis.m
@@ -3,8 +3,8 @@ clear;
 data = load('2022-09_sensor-wind-p1.csv');
 hypothesis = load('2022-09_sensor-hypothesis.csv');
 
-x_data = data(:,1)
-y_data = data(:,3)
+x_data = data(:,1);
+y_data = data(:,3);
 
 m = length(hypothesis(:,2));
 
@@ -14,6 +14,7 @@ y_m = hypothesis(m, 2);
 h = y_0 - (y_0 - y_m) * x;
 
 figure;
-plot(x_data, y_data, 'x')
+plot(x_data, y_data, 'x');
 hold on;
 plot(x, h);
+
